689539d791c9afb552f11cec13a18470db72893a
pierre <pierrecrochet03@gmail.com>
2022-12-16 10:46:47 +0100

    + refactor(migration_v5): useless space

--- src/main/resources/db/migration/V20221213.05__adding_data.sql
+++ src/main/resources/db/migration/V20221213.05__adding_data.sql
@@ -2,9 +2,9 @@
 
 
 
-INSERT INTO address (address_id, street_number,street_name,zipcode,locality,country) VALUES (1,'78' , 'Albert Street', '7800', 'Brussels', 'Belgium' );
-INSERT INTO address (address_id, street_number,street_name,zipcode,locality,country, is_private) VALUES (2,'1' , 'Duc Strassen', '4521', 'Luxembourg', 'Luxembourg', false );
-INSERT INTO person (person_id, firstname,lastname) VALUES (1,'Pierre', 'Test' );
+INSERT INTO address (address_id, street_number,street_name,zipcode,locality,country) VALUES (1, '78' , 'Albert Street', '7800', 'Brussels', 'Belgium' );
+INSERT INTO address (address_id, street_number,street_name,zipcode,locality,country, is_private) VALUES (2, '1' , 'Duc Strassen', '4521', 'Luxembourg', 'Luxembourg', false );
+INSERT INTO person (person_id, firstname,lastname) VALUES (1, 'Pierre', 'Test' );
 INSERT INTO person (person_id, firstname, secondname,lastname) VALUES (2, 'Tom', 'Jerry', 'qwerty' );
 --
 --INSERT INTO person_address (address_id, person_id) VALUES (1,1 );
