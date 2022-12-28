fdaa25c9dc76231fb6346af24f369f89f44cba08
pierre <pierrecrochet03@gmail.com>
2022-12-28 11:26:53 +0100

    feat(QRCodeService): generate a qr-code based on an url

--- src/main/java/api/addressbook/controller/QRCodeController.java
+++ src/main/java/api/addressbook/controller/QRCodeController.java
@@ -5,6 +5,7 @@ import api.addressbook.entity.QRCodeEntity;
 import api.addressbook.repository.AddressRepository;
 import api.addressbook.repository.QRCodeRepository;
 import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import org.apache.commons.io.IOUtils;
@@ -26,6 +27,8 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.util.Optional;
+
+import static api.addressbook.service.QRCodeGeneratorService.generateQRCodeImage;
 
 @Controller
 @RequestMapping("/qr-code")
@@ -59,4 +62,31 @@ public class QRCodeController {
                 .contentType(MediaType.parseMediaType(MediaType.IMAGE_PNG_VALUE)).
                 body(byteArray);
     }
+
+    @RequestMapping("/create")
+    @GetMapping(value = "/url", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> createQRCode() throws IOException, WriterException {
+        String url1 = "https://www.google.com/maps/place/Av.+de+Mersch+103,+6700+Arlon/@49.6872803,5.8277855,17z/data=!3m1!4b1!4m5!3m4!1s0x47eab048197e1e49:0x51a4775d53f430f8!8m2!3d49.6872803!4d5.8299742";
+        String url = "https://www.google.com/maps/place/Rue+des+D%C3%A9port%C3%A9s+78,+6700+Arlon/@49.6807691,5.8195074,20z/data=!4m5!3m4!1s0x47eab0439f7d694d:0xcf5fa2ab8ea056cd!8m2!3d49.6807691!4d5.8197009";
+ var tmp = generateQRCodeImage(url1, 125,125);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header(HttpHeaders.CONTENT_DISPOSITION)
+                .contentType(MediaType.parseMediaType(MediaType.IMAGE_PNG_VALUE)).
+                body(tmp);
+    }
 }
+
+
+// next steps:
+// part 1
+// creating a qr-code on the tmp cache system and print it postman
+    // creating a qr code with the url of the address: https://medium.com/nerd-for-tech/how-to-generate-qr-code-in-java-spring-boot-134adb81f10d
+// save  the qr code on the db
+//export the qr code when consuming the api
+
+// part 2
+// gnerate a nice concat of the addres to writte it by hand
+
+// part 3
+// improving the readme to explain a bit more the api
+// see for versionning othe the api like v1 , v2 etc...
--- src/main/java/api/addressbook/service/QRCodeGeneratorService.java
+++ src/main/java/api/addressbook/service/QRCodeGeneratorService.java
@@ -1,27 +1,36 @@
 package api.addressbook.service;
 
 
+import api.addressbook.repository.AddressRepository;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 @Service("QRCodeGeneratorService")
 public class QRCodeGeneratorService {
 
-    public static void generateQRCodeImage(String text, int width, int height, String filePath) {
+    public static final Logger logger = LoggerFactory.getLogger(AddressRepository.class);
+
+    public static byte[] generateQRCodeImage(String text, int width, int height) throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        MatrixToImageWriter.writeToStream(bitMatrix, "PNG", out);
+        logger.info("byteArray {}",out.toByteArray() );
+        return out.toByteArray();
+
     }
 
-
-    public static byte[] getQRCodeImage(String text, int width, int height) throws WriterException, IOException {
-
-
-        return null;
-    }
 
 }
