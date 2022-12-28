cc18c0584b92d4ab2f7afb042d6e4a0a737c570d
pierre <pierrecrochet03@gmail.com>
2022-12-28 16:38:52 +0100

    feat(QRCodeController): adding an error if a qr code name is duplicated

--- src/main/java/api/addressbook/service/AddressService.java
+++ src/main/java/api/addressbook/service/AddressService.java
@@ -11,20 +11,19 @@ import org.springframework.stereotype.Service;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Optional;
 
 @Service("AddressService")
 public class AddressService {
 
-    public static final Logger logger = LoggerFactory.getLogger(AddressRepository.class);
+    private static final Logger logger = LoggerFactory.getLogger(AddressService.class);
 
     @Autowired
     private  AddressRepository addressRepository;
     @Autowired
     private PersonRepository personRepository;
 
-    public static String concatAddress(Optional<AddressEntity> addressEntity) throws MalformedURLException {
-        String interpolation = addressEntity.get().getStreetName()+"+"+  addressEntity.get().getStreetNumber()+",+"+addressEntity.get().getZipcode()+"+"+ addressEntity.get().getLocality()+"+"+addressEntity.get().getCountry();
+    public static String concatAddress(AddressEntity addressEntity) throws MalformedURLException {
+        String interpolation = addressEntity.getStreetName()+"+"+  addressEntity.getStreetNumber()+",+"+addressEntity.getZipcode()+"+"+ addressEntity.getLocality()+"+"+addressEntity.getCountry();
         URL url = new URL("https://www.google.com/maps/place/"+interpolation);
          logger.info("URL: {}", new URL(url.toString()));
         return url.toString();
