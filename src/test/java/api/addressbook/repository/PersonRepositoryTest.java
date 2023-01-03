6f221b331daa2f7eb4b10062e51aa2bfffbf9d26
pierre <pierrecrochet03@gmail.com>
2023-01-03 16:42:14 +0100

    fix(): fixing unit test for PersonRepositoryTest

--- src/test/java/api/addressbook/repository/PersonRepositoryTest.java
+++ src/test/java/api/addressbook/repository/PersonRepositoryTest.java
@@ -2,6 +2,7 @@ package api.addressbook.repository;
 
 import api.addressbook.entity.PersonEntity;
 import api.addressbook.mapper.PersonMapper;
+import api.addressbook.model.Address;
 import api.addressbook.model.Person;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.*;
@@ -20,11 +21,10 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 /**
  * tuto with:
@@ -39,112 +39,124 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class PersonRepositoryTest {
 
 //    @Autowired
-//    private PersonMapper personMapper;
-
-    private Person person1, person2, person3, personSaved = Person.builder().build();
-
-    private PersonEntity person4;
+    private PersonMapper personMapper;
+    long  personRepositoryCount = 0;
+    private PersonEntity person1;
     public static final Logger logger = LoggerFactory.getLogger(PersonRepository.class);
     @Autowired
     private PersonRepository personRepository;
 
     @BeforeEach
-    public void setUp(){
+    public void setUp() {
+        personRepositoryCount = personRepository.count();
+//        personMapper = mock(PersonMapper.class);
+//        address = Address.builder()
+//                .addressId(1)
+//                .streetName("1")
+//                .boxNumber(null)
+//                .streetName("test street")
+//                .zipcode("1111")
+//                .locality("Test City")
+//                .country("Test Country")
+//                .isPrivate(false)
+//                .person(null)
+//                .build();
+//        addressSet.add(address);
+//        person1 = Person.builder()
+//                .personId(1)
+//                .firstname("Test")
+//                .lastname(null)
+//                .lastname("Tester")
+//                .address(null)
+//                .build();
+//        person2 = Person.builder()
+//                .personId(1)
+//                .firstname("Test2")
+//                .lastname(null)
+//                .lastname("Tester2")
+//                .address(addressSet)
+//                .build();
+//        person3 = Person.builder()
+//                .personId(1)
+//                .firstname("Alpha")
+//                .lastname("Beta")
+//                .lastname("Gamma")
+//                .address(addressSet)
+//                .build();
 
-        person1 = Person.builder()
-                .personId(1)
-                .firstname("Test")
-                .lastname(null)
-                .lastname("Tester")
-                .address(null)
-                .build();
-        person2 = Person.builder()
-                .personId(1)
-                .firstname("Test2")
-                .lastname(null)
-                .lastname("Tester2")
-                .address(null)
-                .build();
-        person3 = Person.builder()
-                .personId(1)
-                .firstname("Alpha")
-                .lastname("Beta")
-                .lastname("Gamma")
-                .address(null)
-                .build();
-
-        person4 = new PersonEntity(1, "aaaa", "aaa", "aaaa", null);
+        person1 = new PersonEntity(4, "Test", "aaa", "aaaa", null);
+//        person4 = personMapper.toMap(person1);
     }
 
     @DisplayName("Save a person")
     @Test
     @Order(1)
-    @Rollback // not sure if it is usefully bcse the rool back is automatic but good to keep in mind
+    @Rollback
+        // not sure if it is usefully bcse the rool back is automatic but good to keep in mind
     void test_save_person_repository() {
-        PersonEntity personSaved = personRepository.save(person4);
-        Assertions.assertThat(personRepository.count()).isEqualTo(1L);
+
+        PersonEntity personSaved = personRepository.save(person1);
+        logger.info("list repo {}", personRepository.findAll());
+        Assertions.assertThat(personRepository.count()).isEqualTo(personRepositoryCount + 1);
         assertEquals(person1.getFirstname(), personSaved.getFirstname());
     }
 
-//    @DisplayName("Get a person")
-//    @Test
-//    @Order(2)
-//    void test_get_person_per_id_repository() {
+    @DisplayName("Get a person")
+    @Test
+    @Order(2)
+    void test_get_person_per_id_repository() {
 //       int id = personRepository.save(personMapper.toMap(person1)).getPersonId();
-//        PersonEntity personSaved1 = personRepository.findById(id).get();
-//        assertEquals(person1.getPersonId(), personSaved1.getPersonId());
-//    }
-//
-//    @DisplayName("Get list of  persons")
-//    @Test
-//    @Order(3)
-//    void test_findAll_person_repository() {
-//        List<PersonEntity> personEntityList = new ArrayList<>();
+        PersonEntity personSaved1 = personRepository.findById(1).get();
+        assertEquals(1, personSaved1.getPersonId());
+    }
+
+    @DisplayName("Get list of  persons")
+    @Test
+    @Order(3)
+    void test_findAll_person_repository() {
+        List<PersonEntity> personEntityList = new ArrayList<>();
 //        personEntityList.add(personMapper.toMap(person1));
 //        personEntityList.add(personMapper.toMap(person2));
 //        personRepository.saveAll(personEntityList);
-//        personEntityList = personRepository.findAll();
-//        assertEquals(2, personEntityList.size());
-//        assertEquals(person1.getPersonId(), personEntityList.get(0).getPersonId());
-//        assertEquals(person2.getPersonId(), personEntityList.get(1).getPersonId());
-//    }
-//
-//    @DisplayName("Update person's field")
-//    @Test
-//    @Order(4)
-//    @Rollback
-//    void test_update_person_repository() {
-//        personRepository.save(personMapper.toMap(person1));
-//        person1.setFirstname("John");
-//        PersonEntity personUpdated = personRepository.save(personMapper.toMap(person1));
-//        assertEquals("John", personUpdated.getFirstname());
-//    }
-//
-//    @DisplayName("Delete a specific persons")
-//    @Test
-//    @Order(5)
-//    @Rollback
-//    void deletePersonTest() {
-//        PersonEntity personSaved1 = personRepository.save(personMapper.toMap(person1));
-//        personRepository.delete(personSaved1);
-//
-//        PersonEntity personEntity2 = null;
-//        Optional<PersonEntity> optionalPerson2 = personRepository.findById(1);
-//
-//        if (optionalPerson2.isPresent()) {
-//            personEntity2 = optionalPerson2.get();
-//        }
-//        Assertions.assertThat(personEntity2).isNull();
-//    }
-//
-//    @DisplayName("Delete all persons")
-//    @Test
-//    @Order(6)
-//    @Rollback(value = true)
-//    void deleteAllPersonTest() {
-//        personRepository.save(personMapper.toMap(person1));
-//        personRepository.save(personMapper.toMap(person2));
-//        personRepository.deleteAll();
-//        assertEquals(0, personRepository.count());
-//    }
+        personEntityList = personRepository.findAll();
+        assertEquals(personRepositoryCount, personEntityList.size());
+        assertEquals(1, personEntityList.get(0).getPersonId());
+        assertEquals(2, personEntityList.get(1).getPersonId());
+    }
+
+    @DisplayName("Update person's field")
+    @Test
+    @Order(4)
+    @Rollback
+    void test_update_person_repository() {
+        person1.setFirstname("John");
+        PersonEntity personUpdated = personRepository.save(person1);
+        assertEquals("John", personUpdated.getFirstname());
+    }
+
+    @DisplayName("Delete a specific persons")
+    @Test
+    @Order(5)
+    @Rollback
+    void deletePersonTest() {
+        PersonEntity personSaved1 = personRepository.findById(1).get();
+        personRepository.delete(personSaved1);
+
+        PersonEntity personEntity2 = null;
+        Optional<PersonEntity> optionalPerson2 = personRepository.findById(1);
+
+        if (optionalPerson2.isPresent()) {
+            personEntity2 = optionalPerson2.get();
+        }
+        Assertions.assertThat(personEntity2).isNull();
+    }
+
+    @DisplayName("Delete all persons")
+    @Test
+    @Order(6)
+    @Rollback(value = true)
+    void deleteAllPersonTest() {
+        personRepository.deleteAll();
+        assertEquals(0, personRepository.count());
+    }
 }
