b0eed774b5947a3b659b7ec0e8c1609919defd0c
pierre <pierrecrochet03@gmail.com>
2023-01-04 11:21:52 +0100

    fix(): fixing the config error for the Integration testing on controller

--- src/test/java/api/addressbook/controller/AddressControllerTest.java
+++ src/test/java/api/addressbook/controller/AddressControllerTest.java
@@ -5,42 +5,46 @@ import api.addressbook.mapper.AddressMapper;
 import api.addressbook.model.Address;
 import api.addressbook.repository.AddressRepository;
 import api.addressbook.repository.PersonRepository;
-import org.junit.Before;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.testng.Assert;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.jupiter.params.shadow.com.univocity.parsers.conversions.Conversions.string;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(AddressController.class)
+//@WebMvcTest
 @ActiveProfiles("test")
-@AutoConfigureMockMvc
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DisplayName("Integration Testing on Address endpoints ")
 class AddressControllerTest {
-
-    private Address address1, address2,  addressSaved = Address.builder().build();
     public static final Logger logger = LoggerFactory.getLogger(AddressControllerTest.class);
     @Autowired
-    private MockMvc mockMvc; // the error is normal
+    private MockMvc mockMvc;
 
     @MockBean
     PersonController personController;
@@ -54,8 +58,6 @@ class AddressControllerTest {
 
     @MockBean
     PersonRepository personRepository;
-    @Autowired
-    private AddressMapper addressMapper;
 
 //    @BeforeEach
 //    public void setUp() {
@@ -63,29 +65,8 @@ class AddressControllerTest {
 //    }
 
     @BeforeEach
-    public void setUp(){
+    public void init(){
         this.mockMvc = MockMvcBuilders.standaloneSetup(new AddressController()).build();
-
-        address1 = Address.builder()
-                .addressId(1)
-                .streetName("1")
-                .boxNumber(null)
-                .streetName("test street")
-                .zipcode("1111")
-                .locality("Test City")
-                .country("Test Country")
-                .isPrivate(false)
-                .person(null).build();
-        address2 = Address.builder()
-                .addressId(1)
-                .streetName("1")
-                .boxNumber(null)
-                .streetName("test street")
-                .zipcode("1111")
-                .locality("Test City")
-                .country("Test Country")
-                .isPrivate(false)
-                .person(null).build();
     }
 
 
@@ -93,8 +74,10 @@ class AddressControllerTest {
     @Test
     @DisplayName("Ping the address endpoint")
     void getPing() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/address/ping"))
-                .andExpect(status().isAccepted());
+        mockMvc.perform(get("/address/ping"))
+                .andExpect(status().isAccepted())
+                   .andExpect(content().string("AddressController Ping"));
+
     }
 
     @Order(2)
@@ -102,25 +85,28 @@ class AddressControllerTest {
     @DisplayName("testing get all address ")
     void getAllAddresses() throws Exception {
         List<AddressEntity> addressEntityList = new ArrayList<>();
-        addressEntityList.add(addressMapper.toMap(address1));
-        addressEntityList.add(addressMapper.toMap(address2));
+        AddressEntity addressEntity1 = new AddressEntity(1, "1", null, "Test street", "1111", "Test City", "Test Country", false, null);
+        AddressEntity addressEntity2 = new AddressEntity(2, "2", "A", "Test street 2", "2222", "Test City 2", "Test Country 2", true, null);
+        addressEntityList.add(addressEntity1);
+        addressEntityList.add(addressEntity2);
 
         when(addressRepository.findAll()).thenReturn(addressEntityList);
-        mockMvc.perform(MockMvcRequestBuilders.get("/address"))
+        mockMvc.perform(get("/address"))
                 .andExpect(status().isOk())
-                .andExpect(content().json("[" +
-                        "{\"addressId\":1,\"streetNumber\":\"1\",\"boxNumber\":null,\"streetName\":\"Test street\",\"zipcode\":\"1111\",\"locality\":\"Test City\",\"country\":\"Test Country\",\"personEntity\":null,\"private\":false}," +
-                        "{\"addressId\":2,\"streetNumber\":\"2\",\"boxNumber\":\"A\",\"streetName\":\"Test street 2\",\"zipcode\":\"2222\",\"locality\":\"Test City 2\",\"country\":\"Test Country 2\",\"personEntity\":null,\"private\":true}]"
+                .andExpect(content().json(
+                        "[{\"addressId\":1,\"streetNumber\":\"1\",\"boxNumber\":null,\"streetName\":\"Test street\",\"zipcode\":\"1111\",\"locality\":\"Test City\",\"country\":\"Test Country\",\"personEntity\":null,\"private\":false},{\"addressId\":2,\"streetNumber\":\"2\",\"boxNumber\":\"A\",\"streetName\":\"Test street 2\",\"zipcode\":\"2222\",\"locality\":\"Test City 2\",\"country\":\"Test Country 2\",\"personEntity\":null,\"private\":true}]"
                 ));
+
     }
 
     @Order(3)
     @Test
     @DisplayName("testing get a address per id ")
     void getAddressById() throws Exception {
-        when(addressRepository.findById(1)).thenReturn(Optional.of(addressMapper.toMap(address1)));
+        AddressEntity addressEntity1 = new AddressEntity(1, "1", null, "Test street", "1111", "Test City", "Test Country", false, null);
+        when(addressRepository.findById(1)).thenReturn(Optional.of(addressEntity1));
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/address/1"))
+        mockMvc.perform(get("/address/1"))
                 .andExpect(status().isFound())
                 .andExpect(content().json("{\"addressId\":1,\"streetNumber\":\"1\",\"boxNumber\":null,\"streetName\":\"Test street\",\"zipcode\":\"1111\",\"locality\":\"Test City\",\"country\":\"Test Country\",\"personEntity\":null,\"private\":false}"));
     }
@@ -130,8 +116,9 @@ class AddressControllerTest {
     @DisplayName("testing adding a address")
     void createAddress() throws Exception {
         String json = "{\"addressId\":1,\"streetNumber\":\"1\",\"boxNumber\":null,\"streetName\":\"Test street\",\"zipcode\":\"1111\",\"locality\":\"Test City\",\"country\":\"Test Country\",\"personEntity\":null,\"private\":false}";
-        when(addressRepository.save(any(AddressEntity.class))).thenReturn(addressMapper.toMap(address1));
-        when(addressRepository.findById(1)).thenReturn(Optional.of(addressMapper.toMap(address1)));
+        AddressEntity addressEntity1 = new AddressEntity(1, "1", null, "Test street", "1111", "Test City", "Test Country", false, null);
+        when(addressRepository.save(any(AddressEntity.class))).thenReturn(addressEntity1);
+        when(addressRepository.findById(1)).thenReturn(Optional.of(addressEntity1));
         mockMvc.perform(MockMvcRequestBuilders.post("/address/")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
@@ -144,8 +131,9 @@ class AddressControllerTest {
     @DisplayName("testing updating a address")
     void updateAddress() throws Exception {
         String json = "{\"addressId\":1,\"streetNumber\":\"1\",\"boxNumber\":null,\"streetName\":\"Test street\",\"zipcode\":\"1111\",\"locality\":\"Test City\",\"country\":\"Test Country\",\"personEntity\":null,\"private\":false}";
-        when(addressRepository.save(any(AddressEntity.class))).thenReturn(addressMapper.toMap(address1));
-        when(addressRepository.findById(1)).thenReturn(Optional.of(addressMapper.toMap(address1)));
+        AddressEntity addressEntity1 = new AddressEntity(1, "1", null, "Test street", "1111", "Test City", "Test Country", false, null);
+        when(addressRepository.save(addressEntity1)).thenReturn(addressEntity1);
+        when(addressRepository.findById(1)).thenReturn(Optional.of(addressEntity1));
         mockMvc.perform(MockMvcRequestBuilders.put("/address/{id}", 1)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(json))
@@ -154,12 +142,14 @@ class AddressControllerTest {
     }
 
     @Test
+    @Disabled
     void deleteAddress() {
-        Assert.assertTrue(true);
+//        Assert.assertTrue(true);
     }
 
     @Test
+    @Disabled
     void deleteAddressPerID() {
-        Assert.assertTrue(true);
+//        Assert.assertTrue(true);
     }
 }
